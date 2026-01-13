72a3465bee73cfa649433bf51a55b6a49104ea85
nikita <nmkazantsev@gmail.com>
2026-01-13 19:15:23 +0300

    it works

--- sealengine3-m/src/main/java/com/example/sealengine3_m/MainRenderer.java
+++ sealengine3-m/src/main/java/com/example/sealengine3_m/MainRenderer.java
@@ -1,6 +1,8 @@
 package com.example.sealengine3_m;
 
 import static android.opengl.GLES10.GL_DEPTH_TEST;
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glEnable;
 
 import android.opengl.GLES30;
@@ -10,7 +12,7 @@ import com.nikitos.GamePageClass;
 public class MainRenderer extends GamePageClass {
     @Override
     public void draw() {
-
+        glClear(GL_COLOR_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
         GLES30.glClearColor(1, 0, 0, 0.0f);
     }
