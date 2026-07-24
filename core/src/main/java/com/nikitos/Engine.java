a18dfd700fbc038160746df9548436dee7cf72c7
nikita <nmkazantsev@gmail.com>
2026-07-24 20:45:34 +0300

    update version

--- core/src/main/java/com/nikitos/Engine.java
+++ core/src/main/java/com/nikitos/Engine.java
@@ -12,7 +12,7 @@ import com.nikitos.utils.Utils;
 
 public class Engine {
     public static String getVersion() {
-        return "v3.2.4";
+        return "v3.2.5";
     }
 
     public float fps;
